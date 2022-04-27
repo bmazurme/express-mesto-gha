d2f3d7907a4280df38c628f4b2fabd4ff1f3d89f
Bogdan <b@bmazur.ru>
2022-04-27 09:52:04 +0300

    fix: card-controller

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,7 +1,7 @@
 const Card = require('../models/card');
 
 const ERROR_WRONG_DATA_CODE = 400;
-// const ERROR_NOT_FOUND_CODE = 404;
+const ERROR_NOT_FOUND_CODE = 404;
 const ERROR_DEFAULT_CODE = 500;
 
 module.exports.createCard = (req, res) => {
@@ -35,7 +35,13 @@ module.exports.likeCard = (req, res) => Card.findByIdAndUpdate(
   { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
   { new: true },
 )
-  .then((card) => { res.status(200).send(card); })
+  // eslint-disable-next-line consistent-return
+  .then((card) => {
+    if (!card) {
+      return res.status(ERROR_NOT_FOUND_CODE).send({ message: 'карточка не найдена' });
+    }
+    res.status(200).send(card);
+  })
   .catch(() => res.status(ERROR_DEFAULT_CODE).send({ message: 'Произошла ошибка' }));
 
 module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
@@ -43,5 +49,11 @@ module.exports.dislikeCard = (req, res) => Card.findByIdAndUpdate(
   { $pull: { likes: req.user._id } }, // убрать _id из массива
   { new: true },
 )
-  .then((card) => { res.status(200).send(card); })
+  // eslint-disable-next-line consistent-return
+  .then((card) => {
+    if (!card) {
+      return res.status(ERROR_NOT_FOUND_CODE).send({ message: 'карточка не найдена' });
+    }
+    res.status(200).send(card);
+  })
   .catch(() => res.status(ERROR_DEFAULT_CODE).send({ message: 'Произошла ошибка' }));
