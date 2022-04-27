dbd2a34b2a658ab7a4c2ebfe3d677cd9fe78783d
Bogdan <b@bmazur.ru>
2022-04-27 11:19:20 +0300

    fix: card

--- controllers/cards.js
+++ controllers/cards.js
@@ -7,7 +7,7 @@ const ERROR_DEFAULT_CODE = 500;
 module.exports.createCard = (req, res) => {
   const { name, link } = req.body;
 
-  Card.create({ name, link })
+  Card.create({ name, link, owner: req.user._id })
     .then((card) => res.status(200).send(card))
     // eslint-disable-next-line consistent-return
     .catch((err) => {
