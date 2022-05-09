cafd6d7318977e45f3247c74c42b9bafc74e5975
Bogdan <b@bmazur.ru>
2022-05-09 21:16:16 +0300

    try

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -9,14 +9,14 @@ const handleAuthError = (res) => {
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  // const { authorization } = req.headers;
+  const { authorization } = req.headers;
 
-  // if (!authorization || !authorization.startsWith('Bearer ')) {
-  //   return handleAuthError(res);
-  // }
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return handleAuthError(res);
+  }
 
-  // const token = authorization.replace('Bearer ', '');
-  const token = req.cookies ? req.cookies.jwt : '';
+  const token = authorization.replace('Bearer ', '');
+  // const token = req.cookies ? req.cookies.jwt : '';
 
   let payload;
 
