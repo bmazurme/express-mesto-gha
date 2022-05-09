ae4d0f4501675d630abcc579f1bdb5ebeb96dc54
Bogdan <b@bmazur.ru>
2022-05-10 00:42:36 +0300

    fix

--- utils/validator.js
+++ utils/validator.js
@@ -1,12 +1,10 @@
 const { celebrate, Joi } = require('celebrate');
 const { isValidObjectId } = require('mongoose');
-
 const BadRequestError = require('../errors/BadRequestError');
 
 const StringRequired = Joi.string().required();
-
 const reg = /^((http|https):\/\/)?(([A-Z0-9][A-Z0-9_-]*)(\.[A-Z0-9][A-Z0-9_-]*)+)/i;
-const isLink = (link) => reg.test(link);
+// const isLink = (link) => reg.test(link);
 
 const validateObjectId = celebrate({
   params: Joi.object().keys({
