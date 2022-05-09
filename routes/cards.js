5e525159b2e7cc7c7f2910b1da7f6465657890fd
Bogdan <b@bmazur.ru>
2022-05-09 23:17:23 +0300

    fix

--- routes/cards.js
+++ routes/cards.js
@@ -13,7 +13,7 @@ router.post(
   '/cards',
   celebrate({
     body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
+      name: Joi.string().min(2).max(30).required(),
       link: Joi.string().pattern(reg).required(),
     }),
   }),
